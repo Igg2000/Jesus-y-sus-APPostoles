efc715c5d2fe73f27e0135ebad8bfaa5c96f19e8
Igg2000 <ignaciogarcia200@gmail.com>
2024-10-23 19:11:31 +0200

    Primera Version

---
+++ JesusysusAPPostolesProyecto1evaluacion/app/src/main/java/com/example/jesusysusappostoles_proyecto1evaluacion/Activity1.java
@@ -0,0 +1,25 @@
+package com.example.jesusysusappostoles_proyecto1evaluacion;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity1 extends AppCompatActivity {
+
+    private Button btn;
+    private TextView txt;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity1);
+
+
+    }
+
+
+}
