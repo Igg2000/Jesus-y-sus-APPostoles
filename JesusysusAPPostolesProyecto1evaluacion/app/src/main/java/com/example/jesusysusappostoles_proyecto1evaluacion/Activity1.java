b65ae48e6a7e09f0964864bb9d2fb803060399a8
crislinari <criislinarii@gmail.com>
2024-10-25 20:29:30 +0200

    Requisitos2V1
    Hemos hecho casi todo sin Nacho :) Lo que hemos hecho ha sido el contador de accesos

--- JesusysusAPPostolesProyecto1evaluacion/app/src/main/java/com/example/jesusysusappostoles_proyecto1evaluacion/Activity1.java
+++ JesusysusAPPostolesProyecto1evaluacion/app/src/main/java/com/example/jesusysusappostoles_proyecto1evaluacion/Activity1.java
@@ -1,5 +1,6 @@
 package com.example.jesusysusappostoles_proyecto1evaluacion;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,15 +10,36 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity1 extends AppCompatActivity {
-
+    // Hace falta para el contador de accesos
+    private static final String PREFS_NAME = "AppPrefs";
+    private static final String COUNTER_KEY = "LaunchCounter";
+    private int launchCounter;
     private Button btn;
-    private TextView txt;
+    private TextView txtAccesos;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity1);
+        // Referencia al TextView en tu layout para mostrar el contador
+        txtAccesos = findViewById(R.id.counterTextView);
 
+        // Accede a SharedPreferences
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+
+        // Lee el contador de lanzamientos
+        launchCounter = preferences.getInt(COUNTER_KEY, -1);
+
+        // Incrementa el contador
+        launchCounter++;
+
+        // Muestra el contador actualizado en el TextView
+        txtAccesos.setText("Veces accedidas: " + launchCounter);
+
+        // Guarda el contador actualizado en SharedPreferences
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(COUNTER_KEY, launchCounter);
+        editor.apply(); // Aplica los cambios
 
     }
 
